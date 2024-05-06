bff841b72cf28ece503247490194e95687ebca1f
Earl006 <earljoe06@gmail.com>
2024-05-06 15:54:47 +0300

    darkmode, lightmode

---
+++ src/global/Topbar.jsx
@@ -0,0 +1,29 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../theme";
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  const handleToggleColorMode = () => {
+    colorMode.toggleColorMode(); // Toggle color mode when the icon is clicked
+  };
+
+  return (
+    <Box>
+      <IconButton onClick={handleToggleColorMode}>
+        {theme.palette.mode === "dark" ? (
+          <DarkModeOutlinedIcon />
+        ) : (
+          <LightModeOutlinedIcon />
+        )}
+      </IconButton>
+    </Box>
+  );
+};
+
+export default Topbar;
