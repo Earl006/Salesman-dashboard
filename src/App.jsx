013246010f2c8feb810b78ff5e1a8608db609b71
Sharonne24 <sawuor395@gmail.com>
2024-05-07 16:16:58 +0300

    clients

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import Topbar from './global/Topbar';
-// import { Login } from '@mui/icons-material';
+import Topbar from './global/Topbar'
 import Login from './components/Login';
 import Sidebar from './components/Sidebar';
 import UserManagement from './UserManagement/UserManagement';
 import Clients from './Clients/Clients';
-import Products from './Products/Products';
-import Orders from './Orders/Orders';
-import Performance from './Performance/Performance';
-import Feedback from './Feedback/Feedback';
-import VisitPlanning from './Visit Planning/VisitPlanning';
-import TaskManagement from './Task Management/TaskManagement';
-import { Route } from 'react-router-dom';
-import { Dashboard } from '@mui/icons-material';
+// import Products from './Products/Products';
+// import Orders from './Orders/Orders';
+// import Performance from './Performance/Performance';
+// import Feedback from './Feedback/Feedback';
+// import VisitPlanning from './Visit Planning/VisitPlanning';
+// import TaskManagement from './Task Management/TaskManagement';
+ import { Routes, Route } from 'react-router-dom';
+
 
 
 function App() {
@@ -24,18 +23,19 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className='content'>
+          <Sidebar />
+          <main className="content">
             <Topbar />
             <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/UserManagement" element={<UserManagement />} />
-            <Route path="/Clients" element={<Clients />} />
-            <Route path="/Products" element={<Products />} />
-            <Route path="/TaskManagement" element={<TaskManagement />} />
-            <Route path="/VisitPlanning" element={<VisitPlanning />} />
-            <Route path="/Performance" element={<Performance />} />
-            <Route path="/Feedback" element={<Feedback />} />
-            <Route path="/Orders" element={<Orders />} />
+            {/* <Route path="/" element={<Dashboard />} /> */}
+            <Route path="/manage-users" element={<UserManagement />} />
+             <Route path="/clients" element={<Clients />} />
+            {/*<Route path="/products" element={<Products />} />
+            <Route path="/tasks" element={<TaskManagement />} />
+            <Route path="/visits" element={<VisitPlanning />} />
+            <Route path="/performance" element={<Performance />} />
+            <Route path="/feedback" element={<Feedback />} />
+            <Route path="/orders" element={<Orders />} /> */}
            {/* <Login />   */}
            </Routes>
           </main>
